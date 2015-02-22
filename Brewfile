3d02e53dca2b2ff34977797fd0c01741f936b1a5
takanamito <takanamito0928@gmail.com>
2015-02-22 21:49:51 +0900

    Brewfileから読み込むように変更

---
+++ Brewfile
@@ -0,0 +1,77 @@
+update
+upgrade
+
+tap phinze/cask
+tap homebrew/dupes
+tap homebrew/versions
+tap josegonzalez/php
+
+install brew-cask
+install autoconf
+install automake
+install bash
+install cmake
+install composer
+install faac
+install freetype
+install gettext
+install git
+install icu4c
+install imagemagick
+install jpeg
+install lame
+install libevent
+install libgpg-error
+install libksba
+install libpng
+install libtool
+install libvo-aacenc
+install libyaml
+install mcrypt
+install memcached
+install mercurial
+install mhash
+install mogenerator
+install mysql55
+install nginx
+install php55
+install php55-apcu
+install openssl
+install ossp-uuid
+install pcre
+install pkg-config
+install readline
+install reattach-to-user-namespace
+install redis
+install tmux
+install vim
+install wget
+install x264
+install xz
+install yasm
+install zlib
+
+cask install google-chrome
+cask install firefox
+cask install iterm2
+cask install sequel-pro
+cask install intellij-idea
+cask install alfred
+cask install bettertouchtool
+cask install firefox
+cask install sublime-text
+cask install kindle
+cask install karabiner
+cask install seil
+cask install google-japanese-ime
+cask install vagrant
+cask install virtualbox
+cask install openoffice
+cask install seashore
+cask install skype
+cask install vlc
+cask install flash-player
+cask install macdown
+cask install dropbox
+
+cleanup
--- install.sh
+++ install.sh
@@ -1,86 +1,17 @@
-#!/bin/sh -e
+#!/bin/sh
 
 # command line tool
 xcode-select --install
 
 # homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+if ! which -s brew; then
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
 
-brew update
-brew upgrade
-
-brew tap phinze/cask
-brew tap homebrew/dupes
-brew tap homebrew/versions
-brew tap josegonzalez/php
-
-brew install brew-cask
-
-brew install autoconf
-brew install automake
-brew install bash
-brew install cmake
-brew install composer
-brew install faac
-brew install freetype
-brew install gettext
-brew install git
-brew install icu4c
-brew install imagemagick
-brew install jpeg
-brew install lame
-brew install libevent
-brew install libgpg-error
-brew install libksba
-brew install libpng
-brew install libtool
-brew install libvo-aacenc
-brew install libyaml
-brew install mcrypt
-brew install memcached
-brew install mercurial
-brew install mhash
-brew install mogenerator
-brew install mysql55
-brew install nginx
-brew install php55
-brew install php55-apcu
-brew install openssl
-brew install ossp-uuid
-brew install pcre
-brew install pkg-config
-brew install readline
-brew install reattach-to-user-namespace
-brew install redis
-brew install tmux
-brew install vim
-brew install wget
-brew install x264
-brew install xz
-brew install yasm
-brew install zlib
-
-brew cask install google-chrome
-brew cask install firefox
-brew cask install iterm2
-brew cask install sequel-pro
-brew cask install intellij-idea
-brew cask install alfred
-brew cask install bettertouchtool
-brew cask install firefox
-brew cask install sublime-text
-brew cask install kindle
-brew cask install karabiner
-brew cask install seil
-brew cask install google-japanese-ime
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install openoffice
-brew cask install seashore
-brew cask install skype
-brew cask install vlc
-brew cask install flash-player
-brew cask install macdown
-brew cask install dropbox
-
-brew cleanup
+while read line
+do
+    if [ -n "$line" ]; then
+        echo "${line}"
+        brew $line
+    fi
+done < Brewfile
